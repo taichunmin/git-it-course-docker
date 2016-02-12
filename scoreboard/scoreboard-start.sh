80c38d105783771d92fbb54b7bd4f7a87407c091
taichunmin <taichunmin@gmail.com>
2016-02-13 05:18:35 +0800

    Fix artisan key:generate issue

--- scoreboard/scoreboard-start.sh
+++ scoreboard/scoreboard-start.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # scoreboard-start.sh
 
+cd /var/www/git-it-course-scoreboard
+git pull
+php artisan key:generate
 service apache2 start
 service redis_6379 start
 tail -F /var/log/apache2/access.log
