38697beef655c338f8a2696fa8177f0d9ece20e7
taichunmin <taichunmin@gmail.com>
2016-02-14 10:07:51 +0800

    add github argument

--- client/scoreboard-reporter.sh
+++ client/scoreboard-reporter.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-COMPLETED=`cat /root/.config/git-it/completed.json || echo []`
+COMPLETED=`test -r /root/.config/git-it/completed.json && cat /root/.config/git-it/completed.json || echo []`
 MID=`hostname`
 NAME=`git config --global user.name || hostname`
+GITHUB=`git config --global user.username || echo ''`
 
 curl \
 --data-urlencode "completed=${COMPLETED}" \
 --data-urlencode "mid=${MID}" \
 --data-urlencode "name=${NAME}" \
+--data-urlencode "github=${GITHUB}" \
 http://scoreboard/completed/update
