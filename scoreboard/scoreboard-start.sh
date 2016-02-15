93538de716e7ea01bb87000b9b5e1f625b8bb235
戴均民 <taichunmin@gmail.com>
2016-02-15 12:57:08 +0800

    修改 redis 的安裝方式，改參考 https://github.com/dockerfile/redis/blob/master/Dockerfile

--- scoreboard/scoreboard-start.sh
+++ scoreboard/scoreboard-start.sh
@@ -5,6 +5,6 @@ cd /var/www/git-it-course-scoreboard
 git pull
 php artisan key:generate
 service apache2 start
-service redis_6379 start
+redis-server /etc/redis/redis.conf &
 /usr/sbin/sshd
 tail -F /var/log/apache2/access.log
