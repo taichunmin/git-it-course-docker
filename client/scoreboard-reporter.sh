c773e7b0774537c5065afc07d40945b7efc678c2
戴均民 <taichunmin@gmail.com>
2016-02-16 15:32:20 +0800

    回報 client 是否還可以被認領

--- client/scoreboard-reporter.sh
+++ client/scoreboard-reporter.sh
@@ -4,10 +4,12 @@ COMPLETED=`test -r /root/.config/git-it/completed.json && cat /root/.config/git-
 MID=`hostname`
 NAME=`git config --global user.name || hostname`
 GITHUB=`git config --global user.username || echo ''`
+OWNED=`[ "never" = "$(chage -l root | grep 'Password expires' | sed -r 's/^[^:]+: //')" ] && echo '1' || echo '0'`
 
 curl \
 --data-urlencode "completed=${COMPLETED}" \
 --data-urlencode "mid=${MID}" \
 --data-urlencode "name=${NAME}" \
 --data-urlencode "github=${GITHUB}" \
+--data-urlencode "owned=${OWNED}" \
 http://scoreboard/completed/update
