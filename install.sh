7bee694ed5b77129702f88f69813fa5f9cff669d
戴均民 <taichunmin@gmail.com>
2018-03-07 10:01:52 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ if [ ! "$(docker -v)" ]; then
 fi
 
 if [ ! "$(docker-compose -v)" ]; then
-  sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo curl -L "https://github.com/docker/compose/releases/download/$(curl -sL https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d'"' -f 4)/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
 fi
 
