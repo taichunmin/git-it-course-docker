5489d8fca868c40761460ec36ed4f3a790a7ced2
戴均民 <taichunmin@gmail.com>
2018-03-07 09:59:03 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,24 +7,22 @@ export LANG=C.UTF-8
 export LC_ALL=C.UTF-8
 export LANGUAGE=C.UTF-8
 
-apt-get update -qq && apt-get upgrade -qqy
+sudo apt-get update -qq && sudo apt-get upgrade -qqy
 
 if [ ! "$(docker -v)" ]; then
   curl -sSL get.docker.com | sh
+  sudo usermod -aG docker $USER
 fi
 
 if [ ! "$(docker-compose -v)" ]; then
-  curl -L "https://github.com/docker/compose/releases/download/$(curl -sL https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d'"' -f 4)/docker-compose-$(uname -s)-$(uname -m)" > /usr/local/bin/docker-compose
-  chmod +x /usr/local/bin/docker-compose
+  sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
 fi
-
-apt-get install htop
-apt-get autoremove -y
 
 git submodule update --init
 if [ ! -f webapp/.env ]; then
   cp webapp/.env.example webapp/.env
 fi
 
-docker-compose pull
+sudo docker-compose pull
 docker run --rm -v "$(realpath ./webapp):/root/webapp" node:latest bash -c 'cd /root/webapp && yarn install'
